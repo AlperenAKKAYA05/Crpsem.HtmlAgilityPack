4cb924750bc1fafe958c68c7dd56748890d0bf46
Crpsem <38250510+Crpsem@users.noreply.github.com>
2018-04-10 15:50:44 +0300

    Crpsem.HtmlAgilityPack
    Crpsem.HtmlAgilityPack

---
+++ src/Crpsem.HtmlAgilityPack/NameValuePair.cs
@@ -0,0 +1,35 @@
+// HtmlAgilityPack V1.0
+namespace HtmlAgilityPack
+{
+    internal class NameValuePair
+    {
+        #region Fields
+
+        internal readonly string Name;
+        internal string Value;
+
+        #endregion
+
+        #region Constructors
+
+        internal NameValuePair()
+        {
+        }
+
+        internal NameValuePair(string name)
+            :
+                this()
+        {
+            Name = name;
+        }
+
+        internal NameValuePair(string name, string value)
+            :
+                this(name)
+        {
+            Value = value;
+        }
+
+        #endregion
+    }
+}
